d5a15a61d251f03caa3cc77fafeabf08e07932b6
Luiz Felipe <42307404+Luizfelipess@users.noreply.github.com>
2021-06-26 12:01:51 -0300

    import route

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,17 @@
-
-//importando o express
+//importação
+import "reflect-metadata";
 import express, { request, response } from "express";
 
-//inicializando o express
+import { router } from "./routes";
 
-//@types/express
+import "./database";
+
 const app = express();
 
-/**
- * GET    => Buscar uma informação 
- * POST   => Inserir/criar uma informação
- * PUT    => Alterar uma informaçao
- * DELETE => Removar um dado
- * PATCH  => Alterar uma informação especifica
- */
+//falando pro express que estamos utilizando json
+app.use(express.json());
 
-//criando a primeira rota
-app.get("/test", (request, response) => {
-    //Request => entrando
-    //Response = > saindo
-    return response.send("Ola NLW");
-});
-
-app.post("/test-post", (request, response) => {
-    return response.send("NLW Post");
-});
+app.use(router);
 
 //criando o servidor http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
